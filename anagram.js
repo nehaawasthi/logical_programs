b72ed732c739de4f2e92d4f83c50ae37ca138e31
Neha Awasthi <32508152+nehaawasthi@users.noreply.github.com>
2021-04-30 17:34:35 +0530

    Create anagram.js

---
+++ anagram.js
@@ -0,0 +1,22 @@
+// # logical_programs
+
+// ## check anagram array
+
+ function checkAnagram(arr){
+  var str = arr[0];
+  var c =1
+  for(let i = 1 ; i < arr.length ; i++){
+   var sortString = arr[i].split('').sort().join('');
+     if(sortString == str){
+      c++;
+     }
+  }
+  if(c== arr.length){
+    return true;
+  }
+  else{
+    return false;
+  }  
+}
+
+console.log(checkAnagram(['abcd','dcba','abcd','abcd']));
